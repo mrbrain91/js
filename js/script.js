8c2bead347e5f6e013747dca685eabecc82a9381
Fazlidin <f.yuldashev.tuit@gmail.com>
2022-04-03 20:51:57 +0500

    tsikl

--- js/script.js
+++ js/script.js
@@ -23,25 +23,29 @@
 
 'use strict';
 
-// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//     b = prompt('На сколько оцените его?', ''),
-//     c = prompt('Один из последних просмотренных фильмов?', ''),
-//     d = prompt('На сколько оцените его?', '');
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
 
-// console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+}
+
+
+
+
+console.log(personalMovieDB);
 
 
 // if (4 == 9) {
@@ -79,5 +83,7 @@
 //     default:
 //         console.log('yoq ekan');
 //         break;
+
+// }       break;
 
 // }
